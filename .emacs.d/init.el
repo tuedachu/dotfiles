e5cc6121fccce43fc9881b90a1b0b151e22bc671
tuedachu <tuedachu@gmail.com>
2018-01-11 16:17:27 +0100

    emacs: Add init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,23 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(package-initialize)
+
+(require 'exwm)
+(require 'exwm-config)
+(exwm-config-default)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (exwm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(set-face-attribute 'default nil :foreground "white" :background "black")
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono")) 
