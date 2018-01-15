c23cd02d9b88944790301286bfce43273ca50874
tuedachu <tuedachu@gmail.com>
2018-01-15 15:34:37 +0100

    emacs: Remove default exwm config + misc
    - Remove exwm default configuration
    - Copy "some" of the config from the default config
    - Add a few keybindings
    - Add visualc commands (htop and watch)
    - Add 'ido-mode'
    - Define alias y-or-n

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,13 +5,38 @@
 
 (require 'exwm)
 (require 'exwm-config)
-(exwm-config-default)
+
+(exwm-input-set-key (kbd "<s-return>") 'eshell)
+
+(add-hook 'exwm-update-class-hook
+	  (lambda ()
+	    (exwm-workspace-rename-buffer exwm-class-name)))
+;; 's-r': Reset
+(exwm-input-set-key (kbd "s-r") #'exwm-reset)
+;; 's-w': Switch workspace
+(exwm-input-set-key (kbd "s-w") #'exwm-workspace-switch)
+;; 's-N': Switch to certain workspace
+(dotimes (i 10)
+  (exwm-input-set-key (kbd (format "s-%d" i))
+		      `(lambda ()
+			 (interactive)
+			 (exwm-workspace-switch-create ,i))))
+;; 's-&': Launch application
+(exwm-input-set-key (kbd "s-&")
+		    (lambda (command)
+		      (interactive (list (read-shell-command "$ ")))
+		      (start-process-shell-command command nil command)))
+;; Enable EXWM
+(exwm-enable)
+;; Other configurations
+(exwm-config-misc)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (exwm))))
+ '(package-selected-packages (quote (with-editor org-plus-contrib exwm))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,3 +46,13 @@
 
 (set-face-attribute 'default nil :foreground "white" :background "black")
 (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono")) 
+(setq org-agenda-files '("~/perso/notes.org"))
+
+
+(ido-mode)
+(windmove-default-keybindings)
+
+(add-to-list 'eshell-visual-commands "htop")
+(add-to-list 'eshell-visual-commands "watch")
+
+(defalias 'yes-or-no-p 'y-or-n-p)
