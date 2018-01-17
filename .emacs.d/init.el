fa542b4a18388056870c7eeec78c10fa01e63b4b
tuedachu <tuedachu@gmail.com>
2018-01-17 18:45:53 +0100

    helm: Add config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,31 +3,23 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (package-initialize)
 
+;;(ido-mode)
 (require 'exwm)
 (require 'exwm-config)
 
-(exwm-input-set-key (kbd "<s-return>") 'eshell)
+(add-to-list 'load-path (concat user-emacs-directory "lisp"))
+(load "keybindings")
+(load "config-helm")
+
+(semantic-mode 1)
 
 (add-hook 'exwm-update-class-hook
 	  (lambda ()
 	    (exwm-workspace-rename-buffer exwm-class-name)))
-;; 's-r': Reset
-(exwm-input-set-key (kbd "s-r") #'exwm-reset)
-;; 's-w': Switch workspace
-(exwm-input-set-key (kbd "s-w") #'exwm-workspace-switch)
-;; 's-N': Switch to certain workspace
-(dotimes (i 10)
-  (exwm-input-set-key (kbd (format "s-%d" i))
-		      `(lambda ()
-			 (interactive)
-			 (exwm-workspace-switch-create ,i))))
-;; 's-&': Launch application
-(exwm-input-set-key (kbd "s-&")
-		    (lambda (command)
-		      (interactive (list (read-shell-command "$ ")))
-		      (start-process-shell-command command nil command)))
+
 ;; Enable EXWM
 (exwm-enable)
+
 ;; Other configurations
 (exwm-config-misc)
 
@@ -36,7 +28,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (with-editor org-plus-contrib exwm))))
+ '(package-selected-packages (quote (helm with-editor org-plus-contrib exwm))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -46,13 +38,17 @@
 
 (set-face-attribute 'default nil :foreground "white" :background "black")
 (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono")) 
+
+;Org files used for the agenda 
 (setq org-agenda-files '("~/perso/notes.org"))
 
 
-(ido-mode)
 (windmove-default-keybindings)
 
-(add-to-list 'eshell-visual-commands "htop")
-(add-to-list 'eshell-visual-commands "watch")
+(with-eval-after-load 'em-term
+  (add-to-list 'eshell-visual-commands "htop")
+  (add-to-list 'eshell-visual-commands "watch"))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+
