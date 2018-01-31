0f75e4fc99f2a1c7d0c3dc15e1d2b9359e4afb1f
tuedachu <tuedachu@gmail.com>
2018-01-31 22:59:23 +0100

    mu4e: Add PZ mail account + Add context + Change port for gmail

--- .emacs.d/lisp/config-mu4e.el
+++ .emacs.d/lisp/config-mu4e.el
@@ -1,17 +1,45 @@
 (require 'mu4e)
 
-; Configuration of the smtp server
-(setq send-mail-function (quote smtpmail-send-it))
-(setq smtpmail-smtp-server "smtp.gmail.com")
-(setq smtpmail-smtp-service 25)
+
 
 (setq mu4e-maildir "/home/tuedachu/.cache/mail")
 (setq mu4e-get-mail-command "mbsync -a")
-(setq mu4e-sent-folder "/tuedachu/sent")
-(setq mu4e-trash-folder "/tuedachu/trash")
-(setq mu4e-drafts-folder "/tuedachu/drafts")
 
-(setq mu4e-update-interval 60)
+(setq mu4e-hide-index-messages t)
+
+(setq mu4e-drafts-folder
+      (lambda (msg)
+        (cond
+         ((or (and msg
+		   (string-prefix-p "/perso/" (mu4e-message-field msg :maildir)))
+	      (string= user-mail-address "arnaud.hoffmann90@gmail.com")) "/perso/[Gmail]/.Drafts")
+	 ((or (and msg
+		   (string-prefix-p "/PZ/" (mu4e-message-field msg :maildir)))
+	      (string= user-mail-address "arnaud@petrostreamz.com")) "/PZ/Brouillons")
+         (t "/tuedachu/[Gmail]/.Drafts")))
+      mu4e-sent-folder
+      (lambda (msg)
+        (cond
+         ((or (and msg
+		   (string-prefix-p "/perso/" (mu4e-message-field msg :maildir)))
+	      (string= user-mail-address "arnaud.hoffmann90@gmail.com")) "/perso/[Gmail]/.Sent")
+	 ((or (and msg
+		   (string-prefix-p "/PZ/" (mu4e-message-field msg :maildir)))
+	      (string= user-mail-address "arnaud@petrostreamz.com")) "/PZ/&AMk-l&AOk-ments envoy&AOk-s")
+         (t "/tuedachu/[Gmail]/.Sent")))
+      mu4e-trash-folder
+      (lambda (msg)
+	(cond
+	 ((string-prefix-p "/perso/" (mu4e-message-field msg :maildir))
+	  "/perso/[Gmail]/.Trash")
+	 ((string-prefix-p "/PZ/" (mu4e-message-field msg :maildir))
+	  "/PZ/&AMk-l&AOk-ments supprim&AOk-s")
+	 "/tuedachu/[Gmail]/.Trash")))
+
+(setq mu4e-update-interval 120)
+
+(require 'org-contacts)
+(add-to-list 'org-contacts-files "~/perso/contacts.org")
 
 ; To speed up indexing process
 ;(setq
@@ -24,10 +52,69 @@
 (setq smtpmail-queue-dir "~/.cache/mail/queued") ; location to store the queued messages
 
 ;; Message signature
-(setq mu4e-compose-signature "Arnaud Hoffmann")
 (setq mu4e-compose-signature-auto-include t)
 
 ;; Don't include myself in the reply
 (setq mu4e-compose-dont-reply-to-self t)
 (add-to-list 'mu4e-user-mail-address-list "tuedachu@gmail.com")
+
+(setq mu4e-view-show-addresses t)
+
+(setq send-mail-function 'smtpmail-send-it)
+
+
+;; Context
+(setq mu4e-contexts
+      `( ,(make-mu4e-context
+	   :name "perso"
+	   :match-func (lambda (msg)
+			 (when msg
+			   (mu4e-message-contact-field-matches msg
+							       '(:from :to) "arnaud.hoffmann90@gmail.com")))
+	   :vars '( ( user-mail-address	    . "arnaud.hoffmann90@gmail.com"  )
+		    ( user-full-name	    . "Arnaud Hoffmann" )
+		    ( mu4e-compose-signature . "Arnaud")
+		    (smtpmail-smtp-server .  "smtp.gmail.com")
+		    (smtpmail-smtp-service . 465)
+		    (smtpmail-stream-type . ssl)))
+	 ,(make-mu4e-context
+	   :name "work"
+	   :match-func (lambda (msg)
+			 (when msg
+			   (mu4e-message-contact-field-matches msg
+							       '(:from :to) "tuedachu@gmail.com")))
+	   :vars '( ( user-mail-address	     . "tuedachu@gmail.com" )
+		    ( user-full-name	     . "Arnaud Hoffmann" )
+		    ( mu4e-compose-signature  .
+					      (concat
+					       "Arnaud Hoffmann\n"
+					       "aka tuedachu\n"))
+		    (smtpmail-smtp-server .  "smtp.gmail.com")
+		    (smtpmail-smtp-service . 465)
+		    (smtpmail-stream-type . ssl)))
+	 ,(make-mu4e-context
+	   :name "PZ"
+	   :match-func (lambda (msg)
+			 (when msg
+			   (mu4e-message-contact-field-matches msg
+							       '(:from :to) "arnaud@petrostreamz.com")))
+	   :vars '( ( user-mail-address	     . "arnaud@petrostreamz.com" )
+		    ( user-full-name	     . "Arnaud Hoffmann" )
+		    ( mu4e-compose-signature  .
+					      (concat
+					       "Arnaud Hoffmann\n"
+					       "Senior Petroleum Engineer\n"))
+		    (smtpmail-smtp-server .  "smtp.office365.com")
+		    (smtpmail-smtp-service . 587)))))
+
+;; set `mu4e-context-policy` and `mu4e-compose-policy` to tweak when mu4e should
+;; guess or ask the correct context, e.g.
+
+;; start with the first (default) context;
+;; default is to ask-if-none (ask when there's no context yet, and none match)
+;; (setq mu4e-context-policy 'pick-first)
+
+;; compose with the current context is no context matches;
+;; default is to ask
+;; (setq mu4e-compose-context-policy nil)
 
