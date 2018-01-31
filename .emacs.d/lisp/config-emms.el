1c80467e6a265c6c7c27bfcdd778d273c8d3c898
tuedachu <tuedachu@gmail.com>
2018-01-31 16:55:16 +0100

    emms: Add initial configuration

---
+++ .emacs.d/lisp/config-emms.el
@@ -0,0 +1,11 @@
+;; EMMS
+(require 'emms-setup)
+(emms-all)
+(require 'emms-player-mpv)
+(add-to-list 'emms-player-list 'emms-player-mpv)
+(require 'emms-info-libtag)
+(setq emms-info-functions '(emms-info-libtag))
+(setq emms-source-file-default-directory "~/music")
+(add-to-list 'emms-player-mpv-parameters "--no-audio-display")
+
+
