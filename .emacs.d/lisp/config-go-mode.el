73b1d02f979f52ffcf2190d75759ffbd6b0c25cd
tuedachu <tuedachu@gmail.com>
2018-02-04 22:14:46 +0100

    go-mode: Add company and helm-company

--- .emacs.d/lisp/config-go-mode.el
+++ .emacs.d/lisp/config-go-mode.el
@@ -4,15 +4,25 @@
 (add-hook 'before-save-hook #'gofmt-before-save)
 
 (defun tuedachu-go-mode-setup ()
-  (go-eldoc-setup))
+  (go-eldoc-setup)
+  (set (make-local-variable 'company-backends) '(company-go))
+  (company-mode))
 
 (add-hook 'go-mode-hook 'tuedachu-go-mode-setup)
 
 (define-key go-mode-map (kbd "M-.") 'godef-jump)
 
+(autoload 'helm-company "helm-company")
+(with-eval-after-load 'company
+  (define-key company-mode-map (kbd "<M-tab>") 'helm-company)
+  (define-key company-active-map (kbd "<M-tab>") 'helm-company))
+
+
+(setq company-idle-delay nil)
+
 (require 'company) 
 (require 'company-go)
-(require 'company-helm)
+
 
 
 (provide 'config-go-mode)
