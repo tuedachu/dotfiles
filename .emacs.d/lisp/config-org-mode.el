8ee4c3d8177dc2fd2e227341c311a12d51131dc0
tuedachu <tuedachu@gmail.com>
2019-10-22 18:08:36 +0200

    org-mode: Add function to insert a latex equation

--- .emacs.d/lisp/config-org-mode.el
+++ .emacs.d/lisp/config-org-mode.el
@@ -5,4 +5,34 @@
 
 (define-key org-mode-map (kbd "s-a") 'org-archive-subtree)
 
+(setq eq-counter 0)
+(defun tuedachu/create-equation()
+  (interactive)
+  (setq eq-counter (1+ eq-counter))
+  (insert  "\\begin{equation}\n")
+  (insert  (concat "\\label{eq:" (int-to-string eq-counter) "}\n\n"))
+  (insert "\\end{equation}\n")
+  (previous-line)
+  (previous-line))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 (provide 'config-org-mode)
