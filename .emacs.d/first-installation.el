86e0e50e3c630f1f472648c6a5c922f31c6de582
tuedachu <tuedachu@gmail.com>
2019-10-24 18:17:17 +0200

    First-installation: Update list of packages

--- .emacs.d/first-installation.el
+++ .emacs.d/first-installation.el
@@ -1,10 +1,9 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("org" ."http://orgmode.org/elpa/"))
 (package-initialize)
 
-(setq package-selected-packages '(ws-butler web-mode company-tern tern js2-mode helm-pass helm-company company-go company go-eldoc pdf-tools undo-tree lua-mode markdown-mode helm-mu go-mode emms-player-mpv wgrep-helm wgrep helm-emms transmission fish-completion magit helm-system-packages helm with-editor org-plus-contrib exwm))
-
+(setq package-selected-packages '(auto-complete company-go company-jedi company-tern dash-functional elpy emms-player-mpv exwm find-file-in-project fish-completion flycheck ghub go-eldoc go-mode graphql helm-company company helm-emms emms helm-mu helm-pass helm-pydoc helm-system-packages helm helm-core highlight-indentation htmlize ivy jedi-core epc ctable concurrent js2-mode lua-mode lv magit git-commit magit-popup markdown-mode mu4e-conversation org-plus-contrib paredit password-store auth-source-pass f pdf-tools pinentry pkg-info epl popup py-autopep8 pydoc python-environment deferred pyvenv quack racket-mode faceup s scheme-complete tablist tern transient dash transmission let-alist treepy undo-tree use-package bind-key web-mode wgrep-helm wgrep with-editor async ws-butler xelb yasnippet))
 
 (package-refresh-contents)
 (package-install-selected-packages)
