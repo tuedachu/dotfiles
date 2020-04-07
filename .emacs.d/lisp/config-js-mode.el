e9e15263d96d358dc5445ab0ec2ff1196a6115bf
tuedachu <tuedachu@gmail.com>
2020-04-07 12:45:29 -0700

    js-mode: Fix tern issues

--- .emacs.d/lisp/config-js-mode.el
+++ .emacs.d/lisp/config-js-mode.el
@@ -1,8 +1,8 @@
 ;; Inspired from 'https://emacs.cafe/emacs/javascript/setup/2017/04/23/emacs-setup-javascript.html'
 
-(define-key js2-mode-map (kbd "M-.") (lambda()
-					    (interactive)
-					    (helm-grep-do-git-grep t)))
+;; (define-key js2-mode-map (kbd "M-.") (lambda()
+;;                                        (interactive)
+;;                                        (helm-grep-do-git-grep t)))
 
 
 (require 'company)
@@ -14,6 +14,7 @@
   (company-mode))
 
 (add-hook 'js2-mode-hook 'tuedachu-js-mode-setup)
+(add-hook 'js2-mode-hook 'prettier-js-mode)
 
 (autoload 'helm-company "helm-company")
 (with-eval-after-load 'company
@@ -27,8 +28,15 @@
   (delete-process "Tern"))
 
 ;; Disable completion keybindings, as we use xref-js2 instead
-(define-key tern-mode-keymap (kbd "M-.") nil)
-(define-key tern-mode-keymap (kbd "M-,") nil)
+;; (define-key tern-mode-keymap (kbd "M-.") nil)
+;; (define-key tern-mode-keymap (kbd "M-,") nil)
 
+
+(add-hook 'mmm-mode-hook
+          (lambda ()
+            (set-face-background 'mmm-default-submode-face nil)))
+
+(add-hook 'vue-mode-hook
+          'prettier-js-mode)
 
 (provide 'config-js-mode)
